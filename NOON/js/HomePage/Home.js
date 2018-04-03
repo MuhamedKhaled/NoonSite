8e7c1f17db0a87cf817a43ae611999e6795aa297
muhamedkhaled <mo7mdtions@gmail.com>
2018-04-03 18:00:59 +0200

    homePage without video slider and details section

---
+++ NOON/js/HomePage/Home.js
@@ -0,0 +1,38 @@
+$('.slickSlider').slick({
+  centerMode: true,
+  slidesToShow: 3,
+  arrows: false,
+  slidesToScroll: 1,
+  dots:true,
+  initialSlide:0,
+  cssEase: 'linear',
+  responsive: [
+    {
+      breakpoint: 1000,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
