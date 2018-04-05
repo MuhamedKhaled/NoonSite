9b9d6ce530e3c20c8a04de6eb00ae23106ccf709
muhamedkhaled <mo7mdtions@gmail.com>
2018-04-06 00:04:20 +0200

    edits

--- NOON/js/HomePage/Home.js
+++ NOON/js/HomePage/Home.js
@@ -45,6 +45,15 @@ $('.loop').owlCarousel({
     nav:true,
     loop:true,
     responsive:{
+      0:{
+           items:1,
+       },
+       600:{
+           items:2,
+       },
+       1000:{
+           items:2,
+       }
 
     }
 });
