70f410a73d3fb6b278b7793edd8d2b72a1fa5b13
Mohamed Fouda <hazem.fouda93@gmail.com>
2018-05-10 12:18:59 +0200

    comment v1

--- Noon_Backend/app.js
+++ Noon_Backend/app.js
@@ -6,8 +6,9 @@ var logger = require('morgan');
 const adminMiddleWare = require("./middlewares/adminMiddleWare");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var commentRouter = require('./routes/comments');
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -15,27 +16,30 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(adminMiddleWare);
 app.use('/', indexRouter);
 app.use('/admins', usersRouter);
 
+//temporary for comment till creating the sport
+app.use('/comments', commentRouter);
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
